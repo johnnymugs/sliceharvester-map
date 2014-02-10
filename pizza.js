50afd9ef897a6e9ced8027941732f7678cf64faf
Jonathan Mukai-Heidt <johnnymukai@gmail.com>
2014-02-10 23:48:54 +0900

    Add basic pop ups

--- pizza.js
+++ pizza.js
@@ -6,19 +6,50 @@
   , toProjection = new OpenLayers.Projection("EPSG:900913") // to Spherical Mercator Projection
   , markerSize = new OpenLayers.Size(21,25)
   , markerOffset = new OpenLayers.Pixel(-(markerSize.w/2), -markerSize.h)
-  , pizzaIcon = new OpenLayers.Icon('marker.png', markerSize, markerOffset);
+  , protoPizzaIcon = new OpenLayers.Icon('marker.png', markerSize, markerOffset);
 
   var i, pizzerias = window.pizzerias;
   for (i = 0; i < pizzerias.length; i++) {
+    (function(i) {
     var shop = pizzerias[i]
-    var lonLat = new OpenLayers.LonLat(shop.lng, shop.lat).transform(fromProjection, toProjection);
-    markersLayer.addMarker(new OpenLayers.Marker(lonLat, pizzaIcon.clone()));
+    , lonLat = new OpenLayers.LonLat(shop.lng, shop.lat).transform(fromProjection, toProjection)
+    , pizzaIcon = protoPizzaIcon.clone()
+    , marker = new OpenLayers.Marker(lonLat, pizzaIcon)
+    , stupidBaseStyle = 'position:relative;width:21px;height:25px;';
+
+    marker.events.register('mouseover', marker, function(event) {
+      event.target.setAttribute('style', stupidBaseStyle + 'cursor:pointer');
+    });
+    marker.events.register('mouseout', marker, function() {
+      event.target.setAttribute('style', stupidBaseStyle + 'cursor:default');
+    });
+
+    marker.events.register('mousedown', marker, function() {
+      for (var popupIndex in map.popups) { (function(popup){ popup.destroy() })(map.popups[popupIndex]); }
+
+      var popupContent = shop.name + '<br />' + shop.desc
+      , popup = new OpenLayers.Popup.FramedCloud(
+        shop.name // id
+        , lonLat
+        , null // autocalculate size
+        , popupContent
+        , pizzaIcon // anchor
+        , true // show X close button
+      );
+
+      map.addPopup(popup);
+    });
+
+    markersLayer.addMarker(marker);
+    })(i);
   }
 
-  map.addLayer(new OpenLayers.Layer.Google("Google Hybrid",
-    {type: google.maps.MapTypeId.HYBRID, numZoomLevels: 20}));
+  map.addLayer(new OpenLayers.Layer.Google("GMaps",
+    {type: google.maps.MapTypeId.ROADMAP, numZoomLevels: 20}));
   map.addLayer(markersLayer);
 
-  map.setCenter(lonLat, zoom);
+  var stupidShop = pizzerias[0];
+  var stupidLonLat = new OpenLayers.LonLat(stupidShop.lng, stupidShop.lat).transform(fromProjection, toProjection)
+   map.setCenter(stupidLonLat, zoom);
 })();
 
